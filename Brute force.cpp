d1c67eb36346db94813f26933b50fa5c693c1474
Nishant Raj Lather <nlather.rj@gmail.com>
2021-10-13 22:40:00 +0530

    Removed redundant comments

--- Brute force.cpp
+++ Brute force.cpp
@@ -1,14 +1,9 @@
-/* C++ program to count number of sub-arrays in which
-number of occurrence of x is equal to that of y
-	using brute force */
 #include <bits/stdc++.h>
 using namespace std;
 
 int sameOccurrence(int arr[], int n, int x, int y)
 {
 	int result = 0;
-
-	// Check for each subarray for the required condition
 	for (int i = 0; i <= n - 1; i++) {
 		int ctX = 0, ctY = 0;
 		for (int j = i; j <= n - 1; j++) {
@@ -24,7 +19,6 @@ int sameOccurrence(int arr[], int n, int x, int y)
 	return (result);
 }
 
-// Driver code
 int main()
 {
 	int arr[] = { 1, 2, 2, 3, 4, 1 };
