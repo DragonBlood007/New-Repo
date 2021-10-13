3626ec66459c1dca3d3e44d3c4b548225af8f1e8
codefreak_123 <43454359+naman-150200@users.noreply.github.com>
2021-10-13 22:41:06 +0530

    Update Html_script.py

--- Html_script.py
+++ Html_script.py
@@ -18,7 +18,7 @@ p2 = Vec2D(x,y)
 
 #lambda function to draw the bezier curve based on the paramter 't': 
 #B(t) = (1 - t^2)P0 + 2(1-t)t P1 + t^2 P2 ; 0 <= t <= 1
-bezier = lambda t: p1 + (1 - t)**2 * (p0 - p1) + t**2 * (p2 - p1)
+bezier = lambda t: p1 + (1 - t)**3 * (p0 - p1) + t**2 * (p2 - p1)
 
 turtle = Turtle()print('Enter End Point :')
 x,y = map(int, input().split())
