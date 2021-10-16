a48a967f497ca64481c161fb97165e678f0c6501
Deepanshu <05natsudragneel18@gmail.com>
2021-10-16 13:35:49 +0530

    Update flood.cpp

--- flood.cpp
+++ flood.cpp
@@ -2,6 +2,15 @@
 #include "graphics.h"
 #include<dos.h>
 #include<conio.h>
+
+template <class T>
+void bubbleSort(T a[], int n) {
+    for (int i = 0; i < n - 1; i++)
+        for (int j = n - 1; i < j; j--)
+            if (a[j] < a[j - 1])
+              swap(a[j], a[j - 1]);
+}
+
 void floodfill(intx,inty,intold,intnewcol)
 {
                 int current;
