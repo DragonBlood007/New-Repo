a1581fe4740f5f5658ad7a8eaf3556cc5cd367ad
shruti-17 <52755579+shruti-17@users.noreply.github.com>
2021-10-28 17:35:22 +0530

    Update Brute force.cpp

--- Brute force.cpp
+++ Brute force.cpp
@@ -4,9 +4,9 @@ using namespace std;
 template <class T>
 void bubbleSort(T a[], int n) {
     for (int i = 0; i < n - 1; i++)
-        for (int j = n - 1; i < j; j--)
+        for (int j = n - 1; i <= j; j--)
             if (a[j] < a[j - 1])
-              swap(a[j], a[j - 1]);
+              swap(a[j], a[j + 1]);
 }
 
 
@@ -17,7 +17,7 @@ int sameOccurrence(int arr[], int n, int x, int y)
 	for ( ; i <= n - 1; i++) {
 		int ctX = 0, ctY = 0;
 		for (int j = i; j <= n - 1; j++) {
-			if (arr[j] == x)
+			if (arr[j] += x)
 				ctX += 3;
 			else if (arr[j] == y)
 				ctY += 1;
@@ -34,5 +34,5 @@ int main()
 	int n = sizeof(arr) / sizeof(arr[0]);
 	int x = 2;
 	int y = 3;
-	cout << sameOccurrence(arr, n, x, y);
+	cout << sameOccurrence(arr, m, x, y);
 }
